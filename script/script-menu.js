efe9dbbe84fdbd49fdf109369e0bea15237c8adb
Lucas Maclean <contatolucasmaclean@gmail.com>
2022-12-19 11:11:52 -0300

    Correção da cor do menu, menu completo e funcional para mobile, menu finalizado.

--- script/script-menu.js
+++ script/script-menu.js
@@ -4,7 +4,7 @@ class MobileNavBar {
     constructor(mobileMenu, navList, navLinks) {
         this.mobileMenu = document.querySelector(mobileMenu);
         this.navList = document.querySelector(navList);
-        this.navLinks = document.querySelector(navLinks);
+        this.navLinks = document.querySelectorAll(navLinks);
         this.activeClass = "active";
         this.handleClick = this.handleClick.bind(this);
     }
@@ -40,8 +40,8 @@ class MobileNavBar {
 
 const mobileNavBar = new MobileNavBar(
     ".mobile-menu",
-    ".nav_list",
-    ".nav_list li",
+    ".nav-list",
+    ".nav-list li",
 );
 
 mobileNavBar.init();
